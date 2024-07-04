8215146eb36b0abe1cefd41e69bcea1f5a43bfe5
king-open <kingwang.0624@gmail.com>
2024-07-04 15:52:43 +0800

    使用npm 包管理器 再用 npm run start 来启动

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("Server listening on port 7878");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
         handle_connection(stream);
     }
 }
@@ -17,6 +17,7 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
+    println!("Request: {}", request_line);
 
     let (status_line, filename) = match request_line.as_str() {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
@@ -25,9 +26,21 @@ fn handle_connection(mut stream: TcpStream) {
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = match fs::read_to_string(filename) {
+        Ok(contents) => contents,
+        Err(_) => {
+            let status_line = "HTTP/1.1 404 NOT FOUND";
+            let contents = fs::read_to_string("404.html").unwrap();
+            let length = contents.len();
+            let response = format!(
+                "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+            );
+            stream.write_all(response.as_bytes()).unwrap();
+            return;
+        }
+    };
+    
     let length = contents.len();
-
     let response = format!(
         "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
     );
